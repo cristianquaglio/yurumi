2ae4066edf6dd8eb123c23801c507c2fe96b67bc
Cristian Quagliozzi <cristianquaglio@gmail.com>
2024-08-13 15:05:08 -0300

    feat: Add notifications by email

--- backend/apps/notifications/src/notifications.controller.ts
+++ backend/apps/notifications/src/notifications.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
 import { NotificationsService } from './notifications.service';
+import { NotifyByEmailDto } from './dto/notify-by-email.dto';
 
 @Controller()
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.notificationsService.getHello();
+  @UsePipes(new ValidationPipe())
+  @EventPattern('notify_email')
+  notifyByEmail(@Payload() notifyByEmailDto: NotifyByEmailDto) {
+    this.notificationsService.notifyByEmail(notifyByEmailDto);
   }
 }
