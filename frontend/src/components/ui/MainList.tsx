9c938f431b138321fbec9ee9b37b6610561a1595
Cristian Quagliozzi <cristianquaglio@gmail.com>
2024-09-06 19:47:53 -0300

    fix: Fix links in MainList

--- frontend/src/components/ui/MainList.tsx
+++ frontend/src/components/ui/MainList.tsx
@@ -1,6 +1,6 @@
 // import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Box, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import PeopleIcon from '@mui/icons-material/People';
 import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
 
@@ -13,22 +13,26 @@ export const MainList = () => {
                         to='/dependences'
                         style={{ textDecoration: 'none', color: 'inherit' }}
                     >
-                        <ListItemIcon>
-                            <AccountBalanceOutlinedIcon />
-                        </ListItemIcon>
+                        <Box display='flex'>
+                            <ListItemIcon>
+                                <AccountBalanceOutlinedIcon />
+                            </ListItemIcon>
+                            <ListItemText primary='Dependencias' />
+                        </Box>
                     </Link>
-                    <ListItemText primary='Dependencias' />
                 </ListItemButton>
                 <ListItemButton>
                     <Link
                         to='/users'
                         style={{ textDecoration: 'none', color: 'inherit' }}
                     >
-                        <ListItemIcon>
-                            <PeopleIcon />
-                        </ListItemIcon>
+                        <Box display='flex'>
+                            <ListItemIcon>
+                                <PeopleIcon />
+                            </ListItemIcon>
+                            <ListItemText primary='Usuarios' />
+                        </Box>
                     </Link>
-                    <ListItemText primary='Usuarios' />
                 </ListItemButton>
             </>
         </>
